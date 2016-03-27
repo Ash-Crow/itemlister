c6c09215cb67427ea471623a280ad32c6f09b15a
Sylvain Boissel <sylvain@ashtree.eu>
2016-03-27 15:49:38 +0200

    cleaning *.pyc files

--- controllers/itemlister.py
+++ controllers/itemlister.py
@@ -4,9 +4,9 @@ app = flask.Blueprint("itemlister", __name__)
 
 @app.route("/")
 def homepage():
-        return flask.render_template("itemlister.html", message="hi there!")
+    return flask.render_template("itemlister.html", message="hi there!")
 
 @app.route("/<wiki>/<title>")
 def list_items(wiki, title):
-		message = "You requested the links for page {} on {}.".format(title, wiki)
-        return flask.render_template("itemlister.html", message=message)
+    message = "You requested the links for page {} on {}.".format(title, wiki)
+    return flask.render_template("itemlister.html", message=message)
